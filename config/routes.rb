30eee7387c9f694ded0527de059ddd551e16bd6e
d_sawada <d_sawada@ga-tech.co.jp>
2018-08-10 15:12:56 +0900

    sys_adminのコントローラ、ビューなど生成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/system_admin', as: 'rails_admin'
-  devise_for :sys_admins
+  devise_for :sys_admins, skip: :all
+
+  devise_scope :sys_admin do
+    get    'sign_in'  => 'sys_admins/sessions#new',     as: 'new_sys_admin_session'
+    post   'sign_in'  => 'sys_admins/sessions#create',  as: 'sys_admin_session'
+    delete 'sign_out' => 'sys_admins/sessions#destroy', as: 'destroy_sys_admin_session'
+  end
+
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 end
