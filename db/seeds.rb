ac6ba59935e663a8dc248784fa120231c37a6ab5
d_sawada <d_sawada@ga-tech.co.jp>
2018-08-10 15:23:21 +0900

    sys_adminでrails_adminにログイン、操作、ログアウト可能

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+SysAdmin.delete_all
+SysAdmin.create({email: "sys_admin@email.com", password: "password", password_confimation: "password" })
