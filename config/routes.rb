98e0954a25c2505d7b90e94935ff5b47f570ae92
d_sawada <d_sawada@ga-tech.co.jp>
2018-08-10 15:08:17 +0900

    rails_admin:install

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  mount RailsAdmin::Engine => '/system_admin', as: 'rails_admin'
   devise_for :sys_admins
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 end
