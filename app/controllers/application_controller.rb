7fbe57629862ee1068c2593a3c522ecfdb52f65c
d_sawada <d_sawada@ga-tech.co.jp>
2018-08-17 16:37:01 +0900

    時刻を修正しようとした残骸

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,11 +19,21 @@ class ApplicationController < ActionController::Base
     if sys_admin_signed_in?
       @company_code = "ga0001"
       @company = Company.first
+      @sys_admin = current_sys_admin
       @admin = Admin.first
       @employee = Employee.first
     else
       @admin = current_admin
       @employee = current_employee
+    end
+    if sys_admin_signed_in?
+      @logout_url = destroy_sys_admin_session_url
+      @user_info = "システム管理者" + @sys_admin.email
+    elsif admin_signed_in?
+      @logout_url = destroy_admin_session_url
+      @user_info= "管理者" + @admin.email
+      @logout_url = destroy_employee_session_url
+      @user_info = "従業員" + @employee.email
     end
   end
 end
