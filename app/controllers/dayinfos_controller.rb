d57801206522533c0a3cf1a52576bd543934cc30
d_sawada <d_sawada@ga-tech.co.jp>
2018-08-17 16:37:01 +0900

    時刻を修正しようとした残骸

--- app/controllers/dayinfos_controller.rb
+++ app/controllers/dayinfos_controller.rb
@@ -5,6 +5,8 @@ class DayinfosController < ApplicationController
     if @dayinfo.start.blank?
       @today_stamp = "今日はまだ出勤していません" if @dayinfo.start.blank?
     elsif @dayinfo.end.blank?
+      logger.debug "======"
+      logger.debug @dayinfo.attributes
       @today_stamp = "今日は#{@dayinfo.start.strftime("%H:%M")}に出勤しています"
     else
       @today_stamp = "今日の打刻は#{@dayinfo.start.strftime("%H:%M")} - #{@dayinfo.end.strftime("%H:%M")}でした"
@@ -19,7 +21,10 @@ class DayinfosController < ApplicationController
         if dayinfo.start.present?
           notice = "すでに出勤しています"
         else
-          dayinfo.start = Time.current
+          dayinfo.start = Time.zone.now
+          logger.debug "============"
+          logger.debug Time.zone.now
+          logger.debug dayinfo.start
           notice = "出勤しました"
           dayinfo.save
         end
@@ -29,12 +34,12 @@ class DayinfosController < ApplicationController
         elsif dayinfo.end.present?
           notice = "すでに退勤しています"
         else
-          dayinfo.end = Time.current
+          dayinfo.end = Time.zone.now
           notice = "退勤しました"
+          logger.debug "=========#{dayinfo.attributes}"
           dayinfo.save
         end
       end
-      logger.debug dayinfo.errors.inspect
       redirect_to timecard_path, notice: notice
     end
   end
