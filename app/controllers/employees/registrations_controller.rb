2ae6373f9812a0034c6482ff377cdaab7cfdb1f5
d-sawada <d_sawada@ga-tech.co.jp>
2018-08-22 06:14:49 +0900

    プリコンパイル

--- app/controllers/employees/registrations_controller.rb
+++ app/controllers/employees/registrations_controller.rb
@@ -42,12 +42,12 @@ class Employees::RegistrationsController < Devise::RegistrationsController
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_sign_up_params
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:company_code, :no, :status, :name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:company_code, :no, :status, :name, :remember_me])
   end
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_account_update_params
-    devise_parameter_sanitizer.permit(:account_update, keys: [:company_code, :no, :status, :name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:company_code, :no, :status, :name, :remember_me])
   end
 
   # The path used after sign up.
