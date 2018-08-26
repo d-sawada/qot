4bf402a7e065f3161f288711845c92270ec617a0
d-sawada <d_sawada@ga-tech.co.jp>
2018-08-27 06:50:09 +0900

    tmp

--- app/models/dayinfo.rb
+++ app/models/dayinfo.rb
@@ -5,8 +5,8 @@ class Dayinfo < ApplicationRecord
   validates :date, presence: true
   validates :employee_id, presence: true
 
-  before_create :times_trunc_sec, :aggregate, :apply_template
-  before_update :times_trunc_sec, :aggregate, :apply_template
+  before_create :times_trunc_sec, :apply_template, :aggregate
+  before_update :times_trunc_sec, :apply_template, :aggregate
 
   def times_trunc_sec
     [:pre_start, :pre_end, :start, :end].each do |sym|
@@ -18,6 +18,15 @@ class Dayinfo < ApplicationRecord
       self[days_sym] = 1
       self[times_sym] = (self[end_sym] - self[start_sym]).to_i / 60
       apply_rest(tymes_sym)
+    end
+  end
+  def apply_template
+    pattern = WorkPattern.find(self.employee.emp_status.work_template[[:sun, :mon, :tue, :wed, :thu, :fri, :sat][self.date.wday]])
+    if pattern.present?
+      self.pre_start = pattern.start
+      self.pre_end = pattern.end
+      self.rest_start = pattern.rest_start
+      self.rest_end = pattern.rest_end
     end
   end
   def aggregate
@@ -32,6 +41,9 @@ class Dayinfo < ApplicationRecord
       self.workdays = 1
       if self.pre_start.present? && self.pre_end.present?
         self.worktimes = (([self.end, self.pre_end].min - [self.start, self.pre_start].max).to_i / 60).apply_rest
+        p self.worktimes
+        p self.pre_end
+        p self.pre_start
       else
         self.worktimes = ((self.end - self.start).to_i / 60).apply_rest
       end
@@ -40,13 +52,6 @@ class Dayinfo < ApplicationRecord
       self.holiday_workdays,  self.workdays  = self.workdays,  0
       self.holiday_worktimes, self.worktimes = self.worktimes, 0
     end
-  end
-  def apply_template
-    pattern = WorkPattern.find(self.employee.emp_status.work_template[[:sun, :mon, :tue, :wed, :thu, :fri, :sat][self.date.wday]])
-    self.pre_start = pattern.start
-    self.pre_end = pattern.end
-    self.rest_start = pattern.rest_start
-    self.rest_end = pattern.rest_end
   end
 
   def daily_data
