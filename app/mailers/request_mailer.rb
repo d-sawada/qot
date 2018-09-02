88feeaf5863904f5d71608193b40d9c1255f460b
renne <dtl.134679@gmail.com>
2018-09-02 21:26:11 +0900

    mailerのスタイルを修正

--- app/mailers/request_mailer.rb
+++ app/mailers/request_mailer.rb
@@ -1,10 +1,12 @@
 class RequestMailer < ApplicationMailer
   def request_accepted_mail(email, admin_comment)
     @admin_comment = admin_comment
-    mail to: email, subject: "申請が承認されました", layout: "request_accepted_mail"
+    mail to: email, subject: "申請が承認されました",
+         layout: "request_accepted_mail"
   end
   def request_rejected_mail(email, admin_comment)
     @admin_comment = admin_comment
-    mail to: email, subject: "申請は棄却されました", layout: "request_rejected_mail"
+    mail to: email, subject: "申請は棄却されました",
+         layout: "request_rejected_mail"
   end
 end
--- app/mailers/trial_mailer.rb
+++ app/mailers/trial_mailer.rb
@@ -1,6 +1,8 @@
 class TrialMailer < ApplicationMailer
   def start_trial_mail(admin)
     @admin = admin
-    mail to: @admin.email, subject: "【勤怠管理システムQueen of Time】アカウント発行のご案内", layout: "start_trial_mail"
+    mail to: @admin.email,
+         subject: "【勤怠管理システムQueen of Time】アカウント発行のご案内",
+         layout: "start_trial_mail"
   end
 end
